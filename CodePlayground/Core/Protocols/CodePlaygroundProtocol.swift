b8f6900d7d3527bbd2fcfbea408b02ae5da515da
cruasanya <98823717+cruasanya@users.noreply.github.com>
2024-05-28 13:30:43 +0300

    Added authentification

--- CodePlayground/Core/Protocols/CodePlaygroundProtocol.swift
+++
@@ -1,8 +0,0 @@
-//
-//  CodePlaygroundProtocol.swift
-//  CodePlayground
-//
-//  Created by Александр Новиков on 28.05.2024.
-//
-
-import Foundation
---
+++ CodePlayground/Core/Views/Authentification/LogInView.swift
@@ -0,0 +1,18 @@
+//
+//  LogInView.swift
+//  CodePlayground
+//
+//  Created by Александр Новиков on 28.05.2024.
+//
+
+import SwiftUI
+
+struct LogInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogInView()
+}
