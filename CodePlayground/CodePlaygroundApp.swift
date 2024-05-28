ccd487800089a5a92e408ae2dff30c4f99c237c9
cruasanya <98823717+cruasanya@users.noreply.github.com>
2024-05-28 10:13:52 +0300

    Initial Commit

---
+++ CodePlayground/CodePlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodePlaygroundApp.swift
+//  CodePlayground
+//
+//  Created by Александр Новиков on 28.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct CodePlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
