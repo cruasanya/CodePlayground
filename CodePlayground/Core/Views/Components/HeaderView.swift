5636d52d277c39d4f0b16af0864479c6ff38621d
cruasanya <98823717+cruasanya@users.noreply.github.com>
2024-05-31 10:53:39 +0300

    added some views

---
+++ CodePlayground/Core/Views/Components/HeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  HeaderView.swift
+//  CodePlayground
+//
+//  Created by Александр Новиков on 28.05.2024.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeaderView()
+}
---
+++ CodePlayground/Core/Views/Main/CreatingProjectView.swift
@@ -0,0 +1,18 @@
+//
+//  CreatingProjectView.swift
+//  CodePlayground
+//
+//  Created by Александр Новиков on 28.05.2024.
+//
+
+import SwiftUI
+
+struct CreatingProjectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreatingProjectView()
+}
