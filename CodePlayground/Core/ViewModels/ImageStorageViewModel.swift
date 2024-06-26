808e35fcfaa44baaf979eadf0371b62a82f36b3a
cruasanya <98823717+cruasanya@users.noreply.github.com>
2024-06-26 18:30:45 +0300

    feat: add save function for projects

---
+++ CodePlayground/Core/ViewModels/ImageStorageViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ImagesStorageViewModel.swift
+//  CodePlayground
+//
+//  Created by Александр Новиков on 26.06.2024.
+//
+
+import Foundation
+import SwiftUI
+import FirebaseStorage
+
+class ImagesStorageViewModel: ObservableObject {
+    @Published var image
+}
---
+++ CodePlayground/Core/Views/Playground/AddCharacterView.swift
@@ -0,0 +1,18 @@
+//
+//  CharactersListView.swift
+//  CodePlayground
+//
+//  Created by Александр Новиков on 24.06.2024.
+//
+
+import SwiftUI
+
+struct AddCharacterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddCharacterView()
+}
